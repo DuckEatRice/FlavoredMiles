d15095be72a6f2bb53e1e9a9cc6ea81c324e9700
DucksEatRice <26echang@stratfordschools.net>
2024-03-27 15:57:21 -0700

    Ethan and Vivan Date: Tuesday, March 26th Changes: No changes really

--- app/src/main/java/com/example/flavoredmiles/MainActivity.java
+++ app/src/main/java/com/example/flavoredmiles/MainActivity.java
@@ -1,17 +1,108 @@
 package com.example.flavoredmiles;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.app.*;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerAdapter recyclerAdapter;
+    private RecyclerView recyclerView;
+    private ArrayList<JSONFile> FoodsList = new ArrayList<>();
+
+
+    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*String temp = loadJSONFromAsset();
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerAdapter = new RecyclerAdapter(this, FoodsList, MainActivity.this);
+        recyclerView.setAdapter(recyclerAdapter);
+
+        try{
+
+        } catch ()
+        {
+
+        }*/
+
 
 
     }
+
+    /*private void setupJSONArray(JSONArray obj) throws JSONException {
+
+        Intent intent = getIntent();
+
+        for (int i = 0; i<=obj.length() - 1; i++)
+        {
+            JSONObject entry = obj.getJSONObject(i);
+
+            String MealName = entry.getString("MealName");
+            //Drawable MealPicture =
+            String MealDescription = entry.getString("MealDescription");
+            String MealType = entry.getString("MealType");
+            String Rating = entry.getString("MealRating");
+            String Price = entry.getString("MealPrice");
+            String Time = entry.getString("MealTime");
+            String Calories = entry.getString("MealCalories");
+            String Ingredients = entry.getString("MealIngredients");
+
+            FoodsList.add(new JSONFile(MealName, null, MealDescription, MealType, Rating, Price, Time, Calories, Ingredients));
+        }
+
+    }
+
+    public String loadJSONFromAsset()
+    {
+        String json = null;
+        try {
+            InputStream is = getAssets().open("Foods.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+            Log.d("Error", "didn't work");
+            return null;
+        }
+        Log.d("Worked", "It worked");
+        return json;
+    }*/
 }
---
+++ app/src/main/java/com/example/flavoredmiles/RecyclerAdapter.java
@@ -0,0 +1,87 @@
+package com.example.flavoredmiles;
+
+import static android.content.Intent.getIntent;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.ImageView;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
+import android.content.Intent;
+import android.util.*;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.flavoredmiles.JSONFile;
+
+import java.util.ArrayList;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.FoodViewHolder>
+{
+    private Context context;
+    private  ArrayList<JSONFile> javaList = new ArrayList<>();
+    Activity activity;
+
+    public RecyclerAdapter(Context context, ArrayList javaList, Activity activity)
+    {
+        this.context = context;
+        this.javaList = javaList;
+        this.activity = activity;
+    }
+
+    public class FoodViewHolder extends RecyclerView.ViewHolder
+    {
+        private TextView textView;
+        private ImageView imageView;
+        private CardView cardView;
+
+        public FoodViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+
+            /*textView = itemView.findViewById(R.id.name);
+            imageView = itemView.findViewById(R.id.blackStar);
+            cardView = itemView.findViewById(R.id.cardView);*/
+        }
+    }
+
+    @NonNull
+    @Override
+
+    public FoodViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_recycler_adapter, parent, false);
+        return new FoodViewHolder((view));
+    }
+
+    @Override
+
+    public void onBindViewHolder(@NonNull FoodViewHolder holder, @SuppressLint("RecyclerView") int position)
+    {
+        JSONFile item = javaList.get(position);
+
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+
+    public int getItemCount()
+    {
+        return javaList.size();
+    }
+
+}
